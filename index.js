751510fadc380728ac5bc0762552ec771f6bcbf6
MichaelOdawa <115035550+MichaelOdawa@users.noreply.github.com>
2022-10-04 21:57:05 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function add (){
+    let resultA=5*(5+10);
+    let resultB=5*5+10;
+    console.log(resultA);
+    console.log(resultB);
+}
+add();
+
+function subtract (){
+    let a=9-2;
+    let b=9-(2-1)
+    console.log(a)
+    console.log(b)
+}
+subtract();
+
+function multiply (){
+    let a=3*2;
+    console.log(a)
+}
+multiply();
